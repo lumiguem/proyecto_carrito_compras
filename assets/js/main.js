20dc82fe6fab751501a7b1f9df26613a5bddc8d2
Miguel Moreno <miguel.morenor@icloud.com>
2023-05-11 17:58:08 -0500

    js, funciones loader, showMenu y showCart

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+import loader from  './components/loader.js'
+import showMenu from './components/showMenu.js'
+import showCart from './components/showCart.js'
+
+/*UI Elements*/
+/* ocultar loader*/
+loader()
+
+/*Mostrar Menu*/
+showMenu()
+
+/*Mostrar Carrito*/
+showCart()
+
+
+
+
+
